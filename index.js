400386c079bcd4a9f728d85573862d3a683fd56e
Sebastião josé <sebastiaojosela@gmail.com>
2025-04-04 11:26:45 +0100

    update ml5 library

--- index.js
+++ index.js
@@ -1,60 +1,42 @@
+/*
+ * 👋 Hello! This is an ml5.js example made and shared with ❤️.
+ * Learn more about the ml5.js project: https://ml5js.org/
+ * ml5.js license and Code of Conduct: https://github.com/ml5js/ml5-next-gen/blob/main/LICENSE.md
+ *
+ * This example demonstrates separating people from the background with ml5.bodySegmentation.
+ */
+
+let bodySegmentation;
 let video;
-let uNet;
-let bodypix;
-let segmentationImage;
-var videoScale = 30;
-var a_timestamp;
+let segmentation;
 
-const sizing_ = 32;
-
-// Number of columns and rows in our system
-var cols, rows;
+let options = {
+  maskType: "background",
+};
 
 function preload() {
-    neuralNet = ml5.uNet('face');
+  bodySegmentation = ml5.bodySegmentation("SelfieSegmentation", options);
 }
 
 function setup() {
-    createCanvas(640, 480);
-    pixelDensity(1);
-    video = createCapture(VIDEO, () => {
-        a_timestamp = (new Date).getTime()
-        neuralNet.segment(video, gotResult, {outputStride:32});
-    });
-    video.hide();
-    segmentationImage = createImage(width, height);
-}
+  createCanvas(640, 480);
+  // Create the video
+  video = createCapture(VIDEO);
+  video.size(640, 480);
+  video.hide();
 
-function gotResult(error, result) {
-    console.log(result)
-    // return;
-    console.log((new Date).getTime() - a_timestamp);
-    if (error) {
-        console.error(error);
-        return;
-    }
-    segmentationImage.copy(result.mask, 0, 0, result.mask.width, result.mask.height, 0, 0, segmentationImage.width, segmentationImage.height);
-    segmentationImage.filter(BLUR, 2);
-    segmentationImage.filter(THRESHOLD);
-    segmentationImage.loadPixels();
-    let r_width = segmentationImage.width * 4;
-    for (let x = 0; x < r_width; x += 4) {
-        for (let y = 0; y < segmentationImage.height; y++) {
-            let loc = x + y * r_width
-            if (segmentationImage.pixels[loc] == 0) {
-                segmentationImage.pixels[loc + 1] = 255
-            } else {
-                segmentationImage.pixels[loc + 3] = 0
-            }
-        }
-    }
-    segmentationImage.updatePixels();
-    a_timestamp = (new Date).getTime()
-    neuralNet.segment(video, gotResult, {outputStride:32});
+  bodySegmentation.detectStart(video, gotResults);
 }
 
 function draw() {
-    background(0);
-    image(video, 0, 0, width, height)
-    image(segmentationImage, 0, 0);
+  background(0, 0, 255);
+  if (segmentation) {
+    video.mask(segmentation.mask);
+    image(video, 0, 0);
+  }
+}
+
+// callback function for body segmentation
+function gotResults(result) {
+  segmentation = result;
 }
